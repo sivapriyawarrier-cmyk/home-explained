1884bfb7b8c4f4b4d4ab0349930d4da015eea929
sivapriyawarrier-cmyk <sivapriya.warrier@gmail.com>
2026-03-20 15:04:14 +0000

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDFPVX2JMjKhgNf-yQwIBsbjVLXTgN5PVw",
+  authDomain: "home-explained-66b18.firebaseapp.com",
+  projectId: "home-explained-66b18",
+  storageBucket: "home-explained-66b18.firebasestorage.app",
+  messagingSenderId: "266528422846",
+  appId: "1:266528422846:web:bbf98142a4c582ad1ad7ef"
+};
+
+export default firebaseConfig;
